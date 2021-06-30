dc57894d3c85f81f076858f37d6904fec3b591bf
Jaforock <JhonForeroJim@gmail.com>
2021-06-29 21:53:43 -0500

    Front chat aplication

---
+++ json.js
@@ -0,0 +1,16 @@
+let Enemigo = {
+    "color" : "rojo",
+    "lugar" : "tierrita roja",
+    "tipo" : [{
+        "agresivo" : "si",
+        "daño" : "51",
+        "vida" : "4000"
+    },{
+        "agresivo" : "no",
+        "daño" : "61",
+        "vida" : "300"
+    }
+    ],
+
+}
+console.log(Enemigo)
